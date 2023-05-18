568258e30c51636923c3434333f4f2856d351582
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-05-18 13:34:12 -0300

    refact: event repository with redis cache database

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,18 @@
-import { prisma } from '@/config';
+import { prisma, redis } from '@/config';
 
 async function findFirst() {
-  return prisma.event.findFirst();
+  const cacheKey = 'event';
+  const cachedEvent = await redis.get(cacheKey);
+
+  if (cachedEvent) {
+    const event = JSON.parse(cachedEvent);
+    return event;
+  }
+
+  const event = prisma.event.findFirst();
+  redis.setEx(cacheKey, 3600, JSON.stringify(event));
+
+  return event;
 }
 
 const eventRepository = {
